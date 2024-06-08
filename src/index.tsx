8b5ea3a76843c34fd17ffabe0a445c7535dd638a
tenkoh <tenkoh.go@gmail.com>
2024-06-08 11:27:33 +0900

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import { Hono } from "hono";
 import { renderer } from "./renderer";
 
 type Bindings = {
-	MY_VAR: string;
-  MY_KV: KVNamespace;
   IMG_BUCKET: R2Bucket;
 };
 
@@ -12,7 +10,6 @@ const app = new Hono<{ Bindings: Bindings }>();
 // app.use(renderer);
 
 app.get("/", async (c) => {
-	// return c.render(<h1>{c.env.MY_VAR}</h1>);
 	const object = await c.env.IMG_BUCKET.get("image000.png");
 	if (!object) return c.notFound();
 	const data = await object.arrayBuffer();
@@ -21,9 +18,6 @@ app.get("/", async (c) => {
 	return c.body(data, 200, {
 		"Content-Type": contentType,
 	});
-  // await c.env.MY_KV.put("hello", "world");
-  // const name = await c.env.MY_KV.get("hello");
-  // return c.render(<h1>{name}</h1>);
 });
 
 export default app;
